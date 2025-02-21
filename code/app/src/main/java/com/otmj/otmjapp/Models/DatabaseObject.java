ef0700bd08ae14bcd3c44972225e594c85d4e41e
uodelana <odelana@ualberta.ca>
2025-02-21 04:19:01 -0700

    Added some comments

--- code/app/src/main/java/com/otmj/otmjapp/Models/DatabaseObject.java
+++ code/app/src/main/java/com/otmj/otmjapp/Models/DatabaseObject.java
@@ -2,6 +2,10 @@ package com.otmj.otmjapp.Models;
 
 import com.otmj.otmjapp.Helper.FirestoreDB;
 
+/**
+ * Wrapper class for objects that are retrieved from database
+ */
+// TODO: Add Java Docs
 public class DatabaseObject {
     private final String ID;
     private final Entity object;
