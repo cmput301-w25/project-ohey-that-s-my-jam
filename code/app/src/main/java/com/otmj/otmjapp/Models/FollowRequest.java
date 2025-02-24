b676a5a71ce708cb57c5835d3a703805ea3c5083
uodelana <145500503+uodelana@users.noreply.github.com>
2025-02-23 18:48:53 -0700

    Changed to String

--- code/app/src/main/java/com/otmj/otmjapp/Models/FollowRequest.java
+++ code/app/src/main/java/com/otmj/otmjapp/Models/FollowRequest.java
@@ -1,8 +1,7 @@
 package com.otmj.otmjapp.Models;
 
-public class FollowRequest extends Follow{
-    public FollowRequest(int follower, int followee){
-        super(follower, followee);
+public class FollowRequest extends Follow {
+    public FollowRequest(String followerID, String followeeID){
+        super(followerID, followeeID);
     }
-
 }
