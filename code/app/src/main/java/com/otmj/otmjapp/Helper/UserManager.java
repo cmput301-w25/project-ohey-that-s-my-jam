683a11f660956c6ca52a3227679b5e804480c9b7
Jospin <jospin.muhanuzi@gmail.com>
2025-02-26 20:48:22 -0700

    Updated login() so that it uses the updated parameters of getDocuments()

--- code/app/src/main/java/com/otmj/otmjapp/Helper/UserManager.java
+++ code/app/src/main/java/com/otmj/otmjapp/Helper/UserManager.java
@@ -1,8 +1,8 @@
 package com.otmj.otmjapp.Helper;
 
-import com.google.firebase.firestore.auth.User;
+import com.google.firebase.firestore.Filter;
 import com.otmj.otmjapp.Models.DatabaseObject;
-
+import com.otmj.otmjapp.Models.User;
 import java.util.ArrayList;
 
 /**
@@ -22,27 +22,26 @@ public class UserManager {
      * @param enteredPassword the password entered by the user
      * @return the authenticated {@link User} object if login is successful, or {@code null} otherwise
      */
+
     public User login(String enteredUsername, String enteredPassword) {
         final User[] user = {null};
 
-        db.getDocuments(new FirestoreDB.DBCallback() {
+        db.getDocuments(Filter.equalTo("username", enteredUsername), new FirestoreDB.DBCallback<User>() {
             @Override
-            public void onSuccess(ArrayList<DatabaseObject> result) {
-                for (DatabaseObject object : result) {
-                    User potentialMatch = (User) object.getObject();
+            public void onSuccess(ArrayList<DatabaseObject<User>> result) {
+                if (!result.isEmpty()) {
+                    User u = result.get(0).getObject();
 
-                    if(potentialMatch.getUsername().equals(enteredUsername)) {
-                        if(potentialMatch.isPassword(enteredPassword)) {
-                            user[0] = potentialMatch;
-
-                            break;
-                        }
+                    if (u.getPassword().equals(enteredPassword)) {
+                        user[0] = u;
                     }
                 }
             }
 
             @Override
-            public void onFailure(Exception e) {} // might not be necessary
+            public void onFailure(Exception e) {
+                // will be implemented later on when it has been decided what should happen on failure
+            }
         });
 
         return user[0]; // return the authenticated user
