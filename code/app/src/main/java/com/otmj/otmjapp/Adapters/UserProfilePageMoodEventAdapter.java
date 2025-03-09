31f92eb63409ce35d5637ba296b7f56af7f0aa01
uodelana <odelana@ualberta.ca>
2025-03-09 06:09:45 -0600

    Avoid null invocation

--- code/app/src/main/java/com/otmj/otmjapp/Adapters/UserProfilePageMoodEventAdapter.java
+++ code/app/src/main/java/com/otmj/otmjapp/Adapters/UserProfilePageMoodEventAdapter.java
@@ -50,7 +50,9 @@ public class UserProfilePageMoodEventAdapter extends ArrayAdapter<MoodEvent> {
         textView_feeling.setText("Feeling: " + Objects.toString(m.getEmotionalState().getDescription(), ""));
         textView_reason.setText("Reason: " + Objects.toString(m.getReason(), ""));
         textView_trigger.setText("Trigger: " + Objects.toString(m.getTrigger(), ""));
-        textView_socialStatus.setText("Social Situation: " + Objects.toString(m.getSocialSituation().toString(), ""));
+        if (m.getSocialSituation() != null) {
+            textView_socialStatus.setText("Social Situation: " + m.getSocialSituation().toString());
+        }
         return view;
     }
 }
