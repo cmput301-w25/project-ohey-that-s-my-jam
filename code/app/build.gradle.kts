5ee7c20ee75f0e47957c79adb4dee1d13e22b81f
Jospin <jospin.muhanuzi@gmail.com>
2025-03-10 15:35:17 -0600

    Updated gradle file

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -71,8 +71,4 @@ dependencies {
     testImplementation("org.mockito:mockito-core:5.7.0")
     testImplementation("org.mockito:mockito-inline:5.2.0")
     implementation("net.bytebuddy:byte-buddy:1.17.1")
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
