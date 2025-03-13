d75656c2761267a017ee645c3917093125baeca9
Jospin <jospin.muhanuzi@gmail.com>
2025-03-12 21:41:44 -0600

    Implemented privacy-based filtering in MoodEventsManager

--- code/app/src/main/java/com/otmj/otmjapp/Models/MoodEvent.java
+++ code/app/src/main/java/com/otmj/otmjapp/Models/MoodEvent.java
@@ -49,7 +49,6 @@ public class MoodEvent extends DatabaseObject {
         Public,
         Private
     }
-
     private Privacy privacy;
 
     MoodEvent() {
@@ -173,6 +172,14 @@ public class MoodEvent extends DatabaseObject {
         this.imageLink = link;
     }
 
+    public Privacy getPrivacy() {
+        return privacy;
+    }
+
+    public void setPrivacy(Privacy privacy) {
+        this.privacy = privacy;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -185,13 +192,5 @@ public class MoodEvent extends DatabaseObject {
     @Override
     public int hashCode() {
         return Objects.hash(userID, createdDate);
-    }
-
-    public Privacy getPrivacy() {
-        return privacy;
-    }
-
-    public void setPrivacy(Privacy privacy) {
-        this.privacy = privacy;
     }
 }
