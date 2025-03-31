0bf77e099a8a9050696ef9eec0d802226f9a7f6b
uodelana <odelana@ualberta.ca>
2025-03-31 01:48:37 -0600

    Edited toml

--- code/app/src/main/java/com/otmj/otmjapp/API/Auth/SpotifyAPIManager.java
+++ code/app/src/main/java/com/otmj/otmjapp/API/Auth/SpotifyAPIManager.java
@@ -26,7 +26,6 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Objects;
